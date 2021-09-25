9c7b24db33d9e6f016a84e1eb92e83345b2cfa85
Selami Prizreni <freakwent.z@gmail.com>
2021-09-26 00:02:27 +0200

    java

--- java.js/index.js
+++ java.js/index.js
@@ -1,6 +1,10 @@
-var shrimps = 3.50;
-var pizza = 4;
-var Bo = 5;
-var Fanta = 1.50;
-var summ = Number(shrimps) + Number(pizza) + Number(Bo) + Number(Fanta);
-console.log = (summ);
+
+
+function calculateInvoice (starterPrice, maindishPrice, dessertPrice, beveragePrice) {
+    var summ = Number(shrimps) + Number(pizza) + Number(Bo) + Number(Fanta);
+    console.log = (summ);
+}
+var shrimps = "3.50";
+var pizza = "4";
+var Bo = "5";
+var Fanta = "1.50";
