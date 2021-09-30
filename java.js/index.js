bff87c476f46ef326131beccc863ed38b4ddd72d
Selami Prizreni <freakwent.z@gmail.com>
2021-09-30 10:53:17 +0200

    pic

--- java.js/index.js
+++ java.js/index.js
@@ -1,10 +1,11 @@
 
 
 function calculateInvoice (starterPrice, maindishPrice, dessertPrice, beveragePrice) {
-    const shrimps = Number(starterPrice);
+    const shrimps = Number(starterPrice, 3,50);
     const pizza = Number(maindishPrice);
     const Bo = Number(dessertPrice);
     const Fanta = Number(beveragePrice);
     returns (s + m + d + b);
 }
-console.log("summ= Starter : shrimps(3.50€), Main: Pizza(")
+console.log("summ = [Starter : shrimps(3.50€), Main: Pizza(")
+
